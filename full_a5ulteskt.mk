c336bb8ed98f113c9ebd704874d503cac7887179
SerenityS <jins4218@naver.com>
2015-07-19 03:51:35 +0900

    fix typo

--- full_a5ulteskt.mk
+++ full_a5ulteskt.mk
@@ -21,7 +21,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from lettuce device
-$(call inherit-product, device/samsung/s5ulteskt/device.mk)
+$(call inherit-product, device/samsung/a5ulteskt/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a5ulteskt
